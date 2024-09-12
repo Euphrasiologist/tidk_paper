f511e93ae502881852416d1329158642e2ae253b
Max Brown <max.carter-brown@aru.ac.uk>
2024-09-12 10:28:13 +0100

    Add supplementary HTML, and update complexity script.

--- src/complexity.bash
+++ src/complexity.bash
@@ -1,27 +1,42 @@
 # assuming the data is in the directory ../data
 
 # create fasta with more data by duplicating the original fasta ../data/bombus_sylvestris.fa
-for i in {1..2}; do
-    cat ../data/bombus_sylvestris.fa >> ../data/bombus_sylvestris2.fa
-done
 
-for i in {1..3}; do
-    cat ../data/bombus_sylvestris.fa >> ../data/bombus_sylvestris3.fa
-done
+# only create the data here if we pass an argument to the script
+if [ "$1" != "" ]; then
 
-for i in {1..4}; do
-    cat ../data/bombus_sylvestris.fa >> ../data/bombus_sylvestris4.fa
-done
+    for i in {1..2}; do
+        cat ../data/bombus_sylvestris.fa >> ../data/bombus_sylvestris2.fa
+    done
 
-for i in {1..5}; do
-    cat ../data/bombus_sylvestris.fa >> ../data/bombus_sylvestris5.fa
-done
+    for i in {1..3}; do
+        cat ../data/bombus_sylvestris.fa >> ../data/bombus_sylvestris3.fa
+    done
+
+    for i in {1..4}; do
+        cat ../data/bombus_sylvestris.fa >> ../data/bombus_sylvestris4.fa
+    done
+
+    for i in {1..5}; do
+        cat ../data/bombus_sylvestris.fa >> ../data/bombus_sylvestris5.fa
+    done
+fi
+
+echo "tidk explore -m 5 -x 30 --distance 0.001 ../data/bombus_sylvestris..."
 
 time tidk explore -m 5 -x 30 --distance 0.001 ../data/bombus_sylvestris.fa > /dev/null
 time tidk explore -m 5 -x 30 --distance 0.001 ../data/bombus_sylvestris2.fa > /dev/null
 time tidk explore -m 5 -x 30 --distance 0.001 ../data/bombus_sylvestris3.fa > /dev/null
 time tidk explore -m 5 -x 30 --distance 0.001 ../data/bombus_sylvestris4.fa > /dev/null
 time tidk explore -m 5 -x 30 --distance 0.001 ../data/bombus_sylvestris5.fa > /dev/null
+
+echo "tidk explore -m 4 -x 15 --distance 0.001 ../data/bombus_sylvestris..."
+
+time tidk explore -m 4 -x 15 --distance 0.001 ../data/bombus_sylvestris.fa > /dev/null
+time tidk explore -m 4 -x 15 --distance 0.001 ../data/bombus_sylvestris2.fa > /dev/null
+time tidk explore -m 4 -x 15 --distance 0.001 ../data/bombus_sylvestris3.fa > /dev/null
+time tidk explore -m 4 -x 15 --distance 0.001 ../data/bombus_sylvestris4.fa > /dev/null
+time tidk explore -m 4 -x 15 --distance 0.001 ../data/bombus_sylvestris5.fa > /dev/null
 
 mkdir temp
 
